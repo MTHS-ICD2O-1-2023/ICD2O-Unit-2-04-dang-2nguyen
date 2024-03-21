99782fed103cf8ac2c631348abbbc5f959c6dedf
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-21 18:05:08 +0000

    gsdgfsd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
 "use strict"
 

1cb1032ba92bead0ff65d6c6dde7c2d857651480
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-21 18:04:03 +0000

    xfvxdvzd

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculatePermeterAndAreaOfRectangle () {
+  // input
+  const baseoftriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseoftriangle * heightOfTriangle)/2
+  
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+  
+}
